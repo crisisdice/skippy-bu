8613eb8af12d2d88ce37a6883ad54da97cd52c4b
Alex Daily <alexanderdaily001@gmail.com>
2022-02-16 12:36:23 +0100

    models todo

--- src/models/src/index.ts
+++ src/models/src/index.ts
@@ -10,9 +10,31 @@ import {
 } from './methods'
 
 import {
-  User
+  Prisma,
+  Game as IGame,
+  User as IUser
 } from '@prisma/client'
 
 export function createGameState(creator: User, key: string) {
   return shuffleDealAndDraw(initalizeGameState(creator, key))
 }
+
+export const routes = {
+  games: 'games',
+  users: 'users',
+}
+
+import { GameState } from './types'
+
+export type Game = Omit<IGame, 'state'> & { state: GameState }
+
+export type Token = {
+  key: string
+  iat: number,
+  exp: number,
+  iss: string
+}
+
+export type User = IUser
+
+export type GameCreateInput = Prisma.GameCreateInput
